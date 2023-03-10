e29f9b214ef8a94156c51a2474e37545eea84ebc
Roque98 <41236879+Roque98@users.noreply.github.com>
2023-03-09 19:36:46 -0600

    Fin de la clase 6

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BtnComponent implements OnInit {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color : 'primary' | 'secondary' | 'success' = 'primary';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get colors() {
+    return {
+      'bg-success-700': this.color === 'success',
+      'hover: bg-success-800': this.color === 'success',
+      'focus: ring-success-300': this.color === 'success',
+      'bg-primary-700': this.color === 'primary',
+      'hover: bg-primary-800': this.color === 'primary',
+      'focus: ring-primary-300': this.color === 'primary',
+      'bg-secondary-700': this.color === 'secondary',
+      'hover: bg-secondary-800': this.color === 'secondary',
+      'focus: ring-secondary-300': this.color === 'secondary',
+    };
+  }
 }
