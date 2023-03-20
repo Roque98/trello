098e779568f53fda0800855231c21921d7673d60
Roque98 <41236879+Roque98@users.noreply.github.com>
2023-03-20 11:47:05 -0600

    Fin de la clase 13

--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,11 +1,14 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ToDo } from 'src/app/components/models/todo.model';
+
+
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styles: [`
+  styles: [
+    `
     /* Animate items as they're being sorted. */
     .cdk-drop-list-dragging .cdk-drag {
       transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
@@ -15,37 +18,72 @@ import { ToDo } from 'src/app/components/models/todo.model';
     .cdk-drag-animating {
       transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
     }
-  `]
+    `
+  ]
 })
 export class BoardComponent implements OnInit {
-
 
   todos: ToDo[] = [
     {
       id: '1',
-      title: 'Learn Angular'
+      title: 'Make dishes'
     },
     {
       id: '2',
-      title: 'Learn React'
-    },
-    {
-      id: '3',
-      title: 'Learn Vue'
-    },
+      title: 'Buy a unicorn'
+    }
   ];
 
-  constructor() { }
+  doing: ToDo[] = [
+    {
+      id: '3',
+      title: 'Watch Angular Path in Platzi'
+    },
+    {
+      id: '4',
+      title: 'Play video games'
+    },
+    {
+      id: '5',
+      title: 'Play soccer'
+    }
+  ];
+
+  done: ToDo[] = [
+    {
+      id: '6',
+      title: 'Go to the gym'
+    },
+    {
+      id: '7',
+      title: 'Go to the movies'
+    },
+  ];
+    
+
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
 
-  drop(event: CdkDragDrop<any[]>) {
-    // Hacer un console log de todas las variables que se pueden usar del event
-    console.log(event);
-    moveItemInArray(this.todos, event.previousIndex, event.currentIndex)
+  drop(event: CdkDragDrop<ToDo[]>) {
+    if (event.previousContainer === event.container) {
+      console.log('same container');
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      console.log('different container');
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
   }
 
-
-
 }
+
